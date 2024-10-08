d3aac97aab588846999d61440495b30da22b0be0
KBOlifant <241299@virtualwindow.co.za>
2024-10-08 22:31:56 +0200

    big boy changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 pageNumber = 1;
-moviesPageAmount = 5;
-ImageURL = "";
-const options = {
+moviesPageAmount = 10;
+MoviesResult = [];
+
+const options_md = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '8ced3a6172msh4420e9f690764b0p152b71jsn3ef5dbc50576',
@@ -9,6 +10,7 @@ const options = {
 	}
 };
 
+<<<<<<< Updated upstream
 
 async function GetMovies(){
     try {
@@ -23,6 +25,74 @@ async function GetMovies(){
 
 
 
+=======
+const options_imdb = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '8ced3a6172msh4420e9f690764b0p152b71jsn3ef5dbc50576',
+		'x-rapidapi-host': 'imdb146.p.rapidapi.com'
+	}
+};
+
+let API_Response;
+
+async function SetMovieData(){
+    try {
+        const response = await fetch(`https://moviesdatabase.p.rapidapi.com/titles?page=${pageNumber}&limit=${moviesPageAmount}`, options_md);
+        API_Response = await response.json();
+        console.log(API_Response);
+        //getImage(result);
+        for (let index = 0; index < moviesPageAmount; index++) {
+            MoviesResult.push(API_Response.results[index].id);
+        }
+        console.log(MoviesResult);
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+class Movies{
+    constructor(name, poster, genre){
+        this.name = name;
+        this.poster = poster;
+        this.genre = genre;  
+    }
+
+    GetMovieName(){
+        //let movieResponse = SetMovieData();
+        async function movieName(){
+            try{
+                let naam = await SetMovieData();
+                console.log(naam.text());
+            }
+            catch(error){
+                console.error(error);
+            }
+        }
+        movieName();
+    }
+    
+}
+
+class Horror extends Movies{
+    constructor(name, poster){
+        super(name, poster);
+        this.genre = "Horror";
+    }
+}
+
+let horror1 = new Horror(API_Response, "33");
+
+//horror1.GetMovieName();
+
+function storeMovieIDs(result){
+    for (let index = 0; index < moviesPageAmount; index++) {
+        MoviesResult.push([result.results[index].id], )
+    }
+}
+
+>>>>>>> Stashed changes
 function getImage(result){
     console.log(result.results[0].primaryImage);
     console.log(result.results[0].primaryImage.url);
@@ -33,4 +103,9 @@ function getImage(result){
     
 }
 
+<<<<<<< Updated upstream
 GetMovies();
+=======
+//GetGenres()
+SetMovieData();
+>>>>>>> Stashed changes
