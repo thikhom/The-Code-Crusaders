ca62e503130af91a346055d314d2c461a6d38430
KBOlifant <241299@virtualwindow.co.za>
2024-10-10 12:13:58 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ pageNumber = 1;
 moviesPageAmount = 10;
 
 image_Buffer = [];
-image_cache = [];
-movie_results_cache = [];
 
 const options_mmd = {
 	method: 'GET',
@@ -13,6 +11,7 @@ const options_mmd = {
 	}
 };
 
+//saves a JSON Object with its unique ID
 function saveJsonObject(jsonObject, ID){
     try{
         localStorage.setItem(ID, JSON.stringify(jsonObject))
@@ -23,6 +22,8 @@ function saveJsonObject(jsonObject, ID){
     }
 }
 
+
+//Retrieves JSON object with ID
 function getJsonObject(ID){
     const item = localStorage.getItem(ID);
     //console.log(JSON.parse(item));
@@ -40,20 +41,32 @@ class Movies{
         async function movieName(){
             try{
                 let result;
+
+                //checking to see if i already have the result stored: to reduce API calls
+
                 if(getJsonObject(String(pageNumber) + movie_Genre) == null)
                 {
+                    //calling API for result as it not stored
                     const response = await fetch(`https://moviesminidatabase.p.rapidapi.com/movie/byGen/${movie_Genre}/`, options_mmd)
                     const _result = await response.json();
                     result = _result;
+
+                     //updating home page images
+                    UpdateImages(result);
+
+                    //saving result for future use
                     saveJsonObject(JSON.stringify(result), String(pageNumber) + movie_Genre);
                 } 
                 else
                 {
+                    //using the already stored result
                     result = getJsonObject(String(pageNumber) + movie_Genre)
+
+                     //updating home page images
+                    UpdateImages(JSON.parse(result));
                 }
                 console.log(result);
-                UpdateImages(JSON.parse(result));
-                
+
             } catch(error){
                 console.log(error);
             }
@@ -73,7 +86,7 @@ class Horror extends Movies{
 class Action extends Movies{
     constructor(name, poster){
         super(name, poster);
-        this.genre = "Adventure";
+        this.genre = "Documentary";
     }
 }
 
@@ -88,6 +101,8 @@ let Horror1 = new Horror(4, "33");
 let Action1 = new Action(4, "33");
 let Comedy1 = new Comedy(4, "33");
 
+//calling the object functions
+
 //Horror1.GetMovieNames(Horror1.genre);
 Action1.GetMovieNames(Action1.genre);
 //Comedy1.GetMovieNames(Comedy1.genre);
@@ -96,13 +111,24 @@ async function UpdateImages(result){
     try{
         let api_result;
         //const JSON_result = JSON.parse(result);
+
+        //looping through the total images on home screen
         for (let index = 0; index < 5; index++) {
+
+            //checking to see if i already have JSON object stored
             if(getJsonObject(result.results[index].imdb_id) == null){
+                //calling API for results
+
                 const response = await fetch(`https://moviesminidatabase.p.rapidapi.com/movie/id/${result.results[index].imdb_id}/`, options_mmd)
                 const api_result_cached = await response.json();
                 api_result = api_result_cached;
+
+                //saving the result for future use
                 saveJsonObject(JSON.stringify(api_result) , result.results[index].imdb_id);
-            } else{
+            } 
+            else{
+                
+                //retrieving the stored JSON object and turning it into something useful
                 let api_result_raw = getJsonObject(result.results[index].imdb_id);
                 api_result = JSON.parse(api_result_raw);
                 console.log(api_result.results.title);
@@ -111,7 +137,8 @@ async function UpdateImages(result){
             image_Buffer.push(api_result.results.banner)
             
         }
-
+        //finally setting the images, using a buffer to load the images in chuncks
+        
         for (let index = 0; index < 5; index++){
             document.getElementsByClassName("flex1")[index].src = image_Buffer[index];
             document.getElementsByClassName("movieAnchor")[index].href = "https://www.imdb.com/title/" + result.results[index].imdb_id + "/";
